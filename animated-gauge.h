92059ca64f17b5c18fee53f740ff22dc488c105f
Samuel <samuel.cuella@gmail.com>
2020-04-08 11:16:35 +0200

    Add color macros

--- animated-gauge.h
+++ animated-gauge.h
@@ -6,6 +6,14 @@
 
 #include "basic-animation.h"
 
+#define SDL_WHITE (SDL_Color){255, 255, 255, SDL_ALPHA_OPAQUE}
+#define SDL_RED (SDL_Color){255, 0, 0, SDL_ALPHA_OPAQUE}
+#define SDL_BLACK (SDL_Color){0, 0, 0, SDL_ALPHA_OPAQUE}
+
+
+#define SDL_UBLACK(surface) (SDL_MapRGB((surface)->format, 0, 0,0))
+#define SDL_UCKEY(surface) (SDL_MapRGB((surface)->format, 255, 0,255))
+
 typedef void (*ValueRenderFunc)(void *self, float value);
 
 typedef struct{
