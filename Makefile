0090fe107dc855f56b86b51bbb435e4ba57d61e6
Samuel <samuel.cuella@gmail.com>
2020-11-24 12:24:36 +0100

    Use FGTape from fg-io as demo data source
    -Replace in-tree FGConnector with fg-io submodule
    -Add switch to use FGTape (default) or FGConnector as
    data source fro the demo/test code

--- Makefile
+++ Makefile
@@ -1,14 +1,24 @@
 #TOP_SRCDIR=..
 #SRCDIR=$(TOP_SRCDIR)/src
 SRCDIR=.
+FG_IO=$(SRCDIR)/fg-io
+FGCONN=$(FG_IO)/flightgear-connector
+FGTAPE=$(FG_IO)/fg-tape
 
 CC=gcc
-CFLAGS=-g3 -O0 `pkg-config sdl2 SDL2_image --cflags` -I$(SRCDIR) -DHAVE_SDL2=1 -DUSE_SDL_GPU=1
+CFLAGS=-g3 -O0 `pkg-config sdl2 SDL2_image --cflags` \
+	   -I$(SRCDIR) \
+	   -I$(FGCONN) \
+	   -I$(FGTAPE) \
+	   -DHAVE_SDL2=1 \
+	   -DUSE_SDL_GPU=1
 LDFLAGS=-lz -lm `pkg-config sdl2 SDL2_image --libs` -Wl,--as-needed -lSDL2_gpu
 EXEC=test-sdl
 #SRC= $(wildcard $(SRCDIR)/*.c)
 SRC= $(filter-out $(SRCDIR)/main.c $(SRCDIR)/testbench.c, $(wildcard $(SRCDIR)/*.c))
 SRC+= sdl-pcf/SDL_GzRW.c sdl-pcf/SDL_pcf.c sdl-pcf/pcfread.c sdl-pcf/utilbitmap.c
+SRC+= $(filter-out $(FGCONN)/fg-connector-test.c, $(wildcard $(FGCONN)/*.c))
+SRC+= $(filter-out $(FGTAPE)/fg-tape-reader.c, $(wildcard $(FGTAPE)/*.c))
 OBJ= $(SRC:.c=.o)
 MAIN_OBJ= main.o
 TEST_OBJ=testbench.o
--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 
 #include "animated-gauge.h"
 #include "basic-hud.h"
-#include "flightgear-connector.h"
 #include "ladder-gauge.h"
 #include "alt-ladder-page-descriptor.h"
 
@@ -20,6 +19,25 @@
 
 #include "roll-slip-gauge.h"
 
+//#define USE_FGCONN 0
+#define USE_FGTAPE 1
+
+#if defined(USE_FGCONN)
+#include "flightgear-connector.h"
+#elif defined(USE_FGTAPE)
+#include "fg-tape.h"
+
+typedef struct{
+    double latitude;
+    double longitude;
+    double altitude;
+    float roll;
+    float pitch;
+    float heading;
+    float airspeed; //kts
+    float vertical_speed; //vertical speed //feets per second
+}TapeRecord;
+#endif
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -28,6 +46,7 @@
 
 BasicHud *hud = NULL;
 
+GPU_Target* gpu_screen = NULL;
 
 float alt = 900.0;
 float odo_val = 0.0;
@@ -147,6 +166,14 @@ int main(int argc, char **argv)
     int i;
     float oldv[5] = {0,0,0,0,0};
 
+#if USE_SDL_GPU
+	GPU_SetRequiredFeatures(GPU_FEATURE_BASIC_SHADERS);
+	gpu_screen = GPU_InitRenderer(GPU_RENDERER_OPENGL_2, SCREEN_WIDTH, SCREEN_HEIGHT, GPU_DEFAULT_INIT_FLAGS);
+	if(gpu_screen == NULL){
+        GPU_LogError("Initialization Error: Could not create a renderer with proper feature support for this demo.\n");
+		return 1;
+    }
+#else
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         return 1;
     }
@@ -173,6 +200,7 @@ int main(int argc, char **argv)
     colors[1] = SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00);
     colors[2] = SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0x00);
     colors[3] = SDL_MapRGB(screenSurface->format, 0x11, 0x56, 0xFF);
+#endif
 
     SDL_Rect whole = {0,0,640,480};
     hud = basic_hud_new();
@@ -184,17 +212,50 @@ int main(int argc, char **argv)
     Uint32 acc = 0;
     i = 3;
 
+#if defined(USE_FGCONN)
     FlightgearConnector *fglink;
     fglink = flightgear_connector_new(6789);
     flightgear_connector_set_nonblocking(fglink);
     FlightgearPacket packet;
+#elif defined(USE_FGTAPE)
+    FGTape *tape;
+    FGTapeSignal signals[8];
+    TapeRecord record;
+    int found;
 
+    tape = fg_tape_new_from_file("fg-io/fg-tape/dr400.fgtape");
+//    fg_tape_dump(tape);
+//    exit(0);
+    found = fg_tape_get_signals(tape, signals,
+        "/position[0]/latitude-deg[0]",
+        "/position[0]/longitude-deg[0]",
+        "/position[0]/altitude-ft[0]",
+        "/orientation[0]/roll-deg[0]",
+        "/orientation[0]/pitch-deg[0]",
+        "/orientation[0]/heading-deg[0]",
+        "/velocities[0]/airspeed-kt[0]",
+	    "/velocities[0]/vertical-speed-fps[0]",
+        NULL
+    );
+    printf("TapeRecord: found %d out of %d signals\n",found, 8);
+
+    int start_pos = 120; /*Starting position in the tape*/
+//    start_pos = 0;
+
+#endif
+
+    Uint32 startms, dtms, last_dtms;
+    Uint32 nframes = 0;
+
+    startms = SDL_GetTicks();
     do{
         ticks = SDL_GetTicks();
         elapsed = ticks - last_ticks;
-        acc += elapsed;
+        dtms = ticks - startms + (start_pos * 1000.0);
 
         done = handle_events(elapsed);
+
+#if defined(USE_FGCONN)
         if(flightgear_connector_get_packet(fglink, &packet)){
             basic_hud_set(hud,  5,
                 ALTITUDE, (float)packet.altitude,
@@ -204,15 +265,41 @@ int main(int argc, char **argv)
                 ROLL, packet.roll
             );
         }
+#elif defined(USE_FGTAPE)
+        if(dtms - last_dtms >= (1000/25)){ //One update per 1/25 second
+            fg_tape_get_data_at(tape, dtms / 1000.0, 8, signals, &record);
+            last_dtms = dtms;
+        }
+        basic_hud_set(hud,  5,
+            ALTITUDE, (double)record.altitude,
+            AIRSPEED, (double)record.airspeed,
+            VERTICAL_SPEED, (double)record.vertical_speed,
+            PITCH, (double)record.pitch,
+            ROLL, (double)record.roll
+        );
 
-        SDL_FillRect(screenSurface, NULL, colors[i]);
+#endif
+
+#if USE_SDL_GPU
+        GPU_ClearRGB(gpu_screen, 0x11, 0x56, 0xFF);
+#else
+        SDL_FillRect(screenSurface, NULL, SDL_UFBLUE(screenSurface));
+#endif
+//        SDL_FillRect(screenSurface, NULL, colors[i]);
 
         base_gauge_render(BASE_GAUGE(hud), elapsed, screenSurface, &whole);
-        SDL_UpdateWindowSurface(window);
 
+#if USE_SDL_GPU
+		GPU_Flip(gpu_screen);
+#else
+        SDL_UpdateWindowSurface(window);
+#endif
+        nframes++;
+        acc += elapsed;
         if(elapsed < 200){
             SDL_Delay(200 - elapsed);
         }
+#if 0
         if(acc >= 1000){
             float tmp;
 
@@ -248,17 +335,38 @@ int main(int argc, char **argv)
 
             acc = 0;
         }
+#endif
+        if(acc >= 1000){ /*1sec*/
+            int h,m,s;
 
+            h = dtms/3600000;
+            dtms -= dtms/3600000 * h;
+            m = dtms / 60000;
+            dtms -= 60000 * m;
+            s = dtms / 1000;
+
+            printf("%02d:%02d:%02d Current FPS: %03d\r",h,m,s, (1000*nframes)/elapsed);
+            fflush(stdout);
+            nframes = 0;
+            acc = 0;
+        }
         i %= N_COLORS;
         last_ticks = ticks;
     }while(!done);
 
     basic_hud_free(hud);
+#if defined(USE_FGCONN)
     flightgear_connector_free(fglink);
-
+#elif defined(USE_FGTAPE)
+    fg_tape_free(tape);
+#endif
     resource_manager_shutdown();
+#if USE_SDL_RENDERER
+	GPU_Quit();
+#else
     SDL_DestroyWindow(window);
     SDL_Quit();
+#endif
     return 0;
 
 }
