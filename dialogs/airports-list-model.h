2d36ceb323c6c37fc3f53f559b029911f849f39b
Samuel <samuel.cuella@gmail.com>
2021-12-29 09:09:00 +0100

    Add licence and copyright headers

--- dialogs/airports-list-model.h
+++ dialogs/airports-list-model.h
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Samuel Cuella <samuel.cuella@gmail.com>
+ *
+ * This file is part of SoFIS - an open source EFIS
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
 #ifndef AIRPORTS_LIST_MODEL_H
 #define AIRPORTS_LIST_MODEL_H
 #include "list-model.h"
